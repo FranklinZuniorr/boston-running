6109ec484b280ef6c971e58a9f896d4789031d7a
Franklin <franklinbarbs763@gmail.com>
2024-12-10 00:24:06 -0300

    styles(scss): should set global sass file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
+import './global.scss'
 import { queryClient } from './config/react-query'
 import { Provider } from 'react-redux'
 import { store } from './config/redux/store'
